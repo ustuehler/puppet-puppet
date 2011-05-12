a2d4eb9f0b7969f26f062d896cc12792130111f4
Uwe Stuehler <uwe@bsdx.de>
2011-05-12 23:08:09 +0200

    Expose many more configuration variables as facts
    These variables will be needed if we want to manage the
    puppet.conf file on all operating systems but keep local
    overrides for the most important parameters.  If we didn't
    preserve those variables they would fall back to their
    built-in default value.  The negative side is that all of
    these values will become sticky and cannot be changed for
    just a single Puppet run.

--- lib/facter/puppet_config.rb
+++ lib/facter/puppet_config.rb
@@ -1,23 +1,17 @@
-Facter.add("puppet_manifest") do
-  setcode do
-    Puppet['manifest']
-  end
-end
-
-Facter.add("puppet_modulepath") do
-  setcode do
-    Puppet['modulepath']
-  end
-end
-
-Facter.add("puppet_vardir") do
-  setcode do
-    Puppet['vardir']
-  end
-end
-
-Facter.add("puppet_ssldir") do
-  setcode do
-    Puppet['ssldir']
+['confdir',
+ 'manifest',
+ 'manifestdir',
+ 'templatedir',
+ 'modulepath',
+ 'factpath',
+ 'vardir',
+ 'ssldir',
+ 'logdir',
+ 'rundir',
+].each do |param|
+  Facter.add("puppet_#{param}") do
+    setcode do
+      Puppet[param]
+    end
   end
 end
--- manifests/init.pp
+++ manifests/init.pp
@@ -18,6 +18,8 @@
 # $puppet_manifest:: Runtime value of the 'manifest' configuration variable.
 # $puppet_modulepath:: Runtime value of the 'modulepath' configuration variable.
 # $puppet_vardir:: Runtime value of the 'vardir' configuration variable.
+# $puppet_ssldir:: Runtime value of the 'ssldir' configuration variable.
+# $puppet_...:: Some other configuration variables.
 #
 # See the following section for a list of class variables which may be
 # affected by the above configuration variables.
@@ -37,6 +39,7 @@
 # $puppetdoc:: Full path to the puppetdoc(1) command.
 # $specsdir:: Where the puppet::module_spec() define stores RSpec files.
 # $vardir:: Where Puppet stores dynamic and growing data.
+# $...:: Some other configuration variables.
 class puppet {
   $manifest = $puppet_manifest
   $modulepath = $puppet_modulepath
@@ -47,6 +50,36 @@ class puppet {
     }
     $vardir = $puppet_vardir
   }
+
+  if ! $puppet_ssldir {
+    fail("unable to determine \$ssldir (puppet_ssldir fact missing?)")
+  }
+  $ssldir = $puppet_ssldir
+
+  if ! $puppet_rundir {
+    fail("unable to determine \$rundir (puppet_rundir fact missing?)")
+  }
+  $rundir = $puppet_rundir
+
+  if ! $puppet_templatedir {
+    fail("unable to determine \$templatedir (puppet_templatedir fact missing?)")
+  }
+  $templatedir = $puppet_templatedir
+
+  if ! $puppet_factpath {
+    fail("unable to determine \$factpath (puppet_factpath fact missing?)")
+  }
+  $factpath = $puppet_factpath
+
+  if ! $puppet_confdir {
+    fail("unable to determine \$confdir (puppet_confdir fact missing?)")
+  }
+  $confdir = $puppet_confdir
+
+  if ! $puppet_logdir {
+    fail("unable to determine \$logdir (puppet_logdir fact missing?)")
+  }
+  $logdir = $puppet_logdir
 
   $puppetdoc = "$puppet_bindir/puppetdoc"
 
