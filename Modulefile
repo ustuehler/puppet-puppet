6dea87adbd689f42eb10059572ef88db18f8c4da
Uwe Stuehler <uwe@bsdx.de>
2012-04-21 20:22:05 +0200

    Use ruby::active_record instead of ruby::activerecord

--- Modulefile
+++ Modulefile
@@ -1,4 +1,5 @@
-name 'puppet'
+name 'ustuehler-puppet'
 version '0.0.1'
 
 dependency 'file_concat', '>= 0.0.1'
+dependency 'ruby', '>= 0.0.1'
--- manifests/master/install.pp
+++ manifests/master/install.pp
@@ -1,6 +1,6 @@
 class puppet::master::install
 {
 	#require inline_template("${name}::<%= operatingsystem.downcase %>")
-	require ruby::activerecord
+	require ruby::active_record
 	require ruby::sqlite3
 }
